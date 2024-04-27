70f3476fdc15b59d0aeb8561ecd438c113f25c56
SergiiOst <sergeyostapenko85@gmail.com>
2024-04-27 23:48:57 +0300

    HW7 JS

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputRef = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesRef = document.getElementById("boxes");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = inputRef.value;
+  destroyBoxes();
+
+  const boxes = [];
+  let size = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.cssText = `
+      width: ${size}px;
+      height: ${size}px;
+      background-color: ${getRandomHexColor()};
+      margin-bottom: 10px;
+    `;
+    boxes.push(box);
+    size += 10;
+  }
+
+  boxesRef.append(...boxes);
+  inputRef.value = "";
+}
+
+function destroyBoxes() {
+  boxesRef.innerHTML = "";
+}
