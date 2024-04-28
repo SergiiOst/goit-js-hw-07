30afedeaa6b82bd1444e30a4bd09dd6957dea145
SergiiOst <sergeyostapenko85@gmail.com>
2024-04-28 14:44:28 +0300

    HW 7 JS

--- js/task-6.js
+++ js/task-6.js
@@ -7,13 +7,19 @@ function getRandomHexColor() {
 const inputRef = document.querySelector("input");
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
-const boxesRef = document.getElementById("boxes");
+const boxesRef = document.querySelector("#boxes");
 
-createBtn.addEventListener("click", createBoxes);
+createBtn.addEventListener("click", () => {
+  const amount = Number(inputRef.value);
+  createBoxes(amount);
+});
 destroyBtn.addEventListener("click", destroyBoxes);
 
-function createBoxes() {
-  const amount = inputRef.value;
+function createBoxes(amount) {
+  if (amount < 1 || amount > 100) {
+    alert("Enter a number between 1 and 100");
+    return;
+  }
   destroyBoxes();
 
   const boxes = [];
